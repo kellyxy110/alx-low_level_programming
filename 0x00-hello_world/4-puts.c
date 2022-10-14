25e6c67f695f695ac0647a6280716f4d0a4aa6cb
kellyxy110 <kellyxy110@gmail.com>
2022-10-13 21:03:01 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return : Always 0
- */
+ *
+ *  * main - entry
+ *
+ *   * Return: Always 0
+ *
+ *    */
+
 int main(void)
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
