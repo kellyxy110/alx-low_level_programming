2fa76498a7bee3cdbbf15768f96422e618441ae0
kellyxy110 <kellyxy110@gmail.com>
2022-10-25 12:06:08 -0700

    adds 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,54 +1,29 @@
-include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - Generates random valid passwords for the
- *        program 101-crackme.
- *
- * Return: Always 0.
+ * _puts - prints a string
+ * @str: the string
+ * Return: the length of the string
  */
-int main(void)
+
+void _puts(char *str)
+
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(0));
+		int i = 0;
 
-	while (sum < 2772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
 
-	password[index] = '\0';
 
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
+		while (str[i] != '\0')
 
-		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
-	}
 
-	printf("%s", password);
+			_putchar(str[i]);
 
-	return (0);
+			i++;
+
+		}
+
+		_putchar('\n');
+
 }
