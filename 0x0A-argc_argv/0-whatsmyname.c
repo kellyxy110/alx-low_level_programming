795cce45eb196fcbdcd980943a757df3c9c50dfa
kellyxy110 <kellyxy110@gmail.com>
2022-11-09 19:34:13 -0800

    adds 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 
 /**
  *  main - prints its name, followed by a new line.
- *   @argc: argument count
- *    @argv: arguments
- *    Return: 0
+ *  @argc: argument count
+ *  @argv: arguments
+ *  Return: 0
  */
 int main(int argc, char **argv)
 {
